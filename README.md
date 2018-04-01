37b4b9bbd54bf73f2eb6cb54d0a1888aaa79d529
Clint <clint@greymouse.com.au>
2018-04-01 23:01:40 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Runcloud API
+Runcloud has an API (https://runcloud.io/api/welcome.html), this is a api wrapper class for their API.
---
+++ src/RuncloudAPI.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author      Clint Davis <os-dev@clint.davis.to>
+ * @copyright   Copyright (c) Clint Davis
+ * @license     http://opensource.org/licenses/BSD-3-Clause 3-clause BSD
+ *
+ * @link        https://github.com/ClintDavis/runcloud-api-php
+ */
+
+namespace ClintDavis\RuncloudAPI;
+
+class RuncloudAPI
+{
+
+	
+	/*
+	*	Class constructor
+	 */
+	function __construct() {
+
+	}
+
+
+
+
+}
